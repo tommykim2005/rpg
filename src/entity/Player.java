34103a805a9ea4b6ef7ee1487976b595041ba966
Thomas Kim <tommykim2005@gmail.com>
2025-10-17 22:53:44 -0700

    Added Sound handler (music, sound effects) and UI

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,7 +16,7 @@ public class Player extends Entity {
 
     public final int screenX;
     public final int screenY;
-    int hasKey = 0;
+    public int hasKey = 0;
 
 
 
@@ -122,15 +122,22 @@ public class Player extends Entity {
 
             switch(objectName) {
             case "Key":
+                gp.playSE(1);
                 hasKey++;
                 gp.obj[i] = null;
                 System.out.println("Key: " + hasKey);
                 break;
             case "Door":
+                gp.playSE(3);
                 if(hasKey > 0) {
                     gp.obj[i] = null;
                     hasKey--;
                 }
+                break;
+            case "Boots":
+                gp.playSE(2);
+                speed += 2;
+                gp.obj[i] = null;
                 break;
             }
         }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -26,8 +26,6 @@ public class GamePanel extends JPanel implements Runnable{
     //WORLD SETTINGS
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
 
     // FPS
     int FPS = 60;
@@ -35,9 +33,12 @@ public class GamePanel extends JPanel implements Runnable{
     TileManager tileM = new TileManager(this);
 
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound music = new Sound();
+    Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+    Thread gameThread;
     public Player player = new Player(this,keyH);
     public SuperObject obj[] = new SuperObject[10];
 
@@ -66,6 +67,8 @@ public class GamePanel extends JPanel implements Runnable{
     public void setupGame(){
         
         aSetter.setObject();
+
+        playMusic(0);
 
     }
 
@@ -126,7 +129,25 @@ public class GamePanel extends JPanel implements Runnable{
         }
         player.draw(g2);
 
+        ui.draw(g2);
+
         g2.dispose();
 
+    }
+
+    public void playMusic(int i){
+
+        music.setFile(i);
+        music.play();
+        music.loop();
+
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+
+    public void playSE(int i){
+        se.setFile(i);
+        se.play();
     }
 }
